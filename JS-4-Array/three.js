01f76d14d9e1a973da987e08d7671a2603cf65c7
varun2425 <varunmahi030@gmail.com>
2025-10-16 14:15:32 +0530

    JS

---
+++ JS-4-Array/three.js
@@ -0,0 +1,12 @@
+let eids=[101,102,103,104]
+let employees=[
+               {"eid":101,"ename":"Varun","esal":45000},
+               {"eid":102,"ename":"Annu","esal":54000},
+               {"eid":103,"ename":"Nobitha","esal":65000},
+               {"eid":104,"ename":"vanni","esal":85000}
+]
+
+console.log(employees[0].ename)
+console.log (employees[1].ename)
+console.log (employees[2].ename)
+
