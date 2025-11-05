bcee76197ef0f6f95610e5762147469e6d65bf4d
varun2425 <varunmahi030@gmail.com>
2025-11-05 15:43:33 +0530

    js

---
+++ Assigment-1/static/color.js
@@ -0,0 +1,22 @@
+
+
+function Display1(){
+    // alert("gm")
+    document.getElementById("one").style.backgroundColor = "yellow"
+}
+function Display2(){
+    document.getElementById("two").style.backgroundColor = "yellow"
+}
+function Display3(){
+    document.getElementById("three").style.backgroundColor = "yellow"
+}
+function Display4(){
+    document.getElementById("four").style.backgroundColor = "yellow"
+}
+function Display5(){
+    document.getElementById("five").style.backgroundColor = "yellow"
+}
+function Display6(){
+    let input =document.getElementById("six")
+    input.value=input.value.toUpperCase()
+}
