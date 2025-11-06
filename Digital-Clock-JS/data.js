6a5e3d6ab3468e92a182c161ef0cf8add561cf38
varun2425 <varunmahi030@gmail.com>
2025-11-06 10:44:55 +0530

    js

---
+++ Digital-Clock-JS/data.js
@@ -0,0 +1,9 @@
+setInterval(()=>{
+    document.getElementsByTagName('p')[0].innerHTML=new Date().toLocaleTimeString()
+},1000)
+
+
+
+//let ct=new Date().toLocaleDateString ()
+//document.getElementsByTagName('p')[0].innerHTML=ct;
+//document.getElementsByTagName('p')[0]
