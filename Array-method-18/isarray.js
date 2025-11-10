28aadf11d1e47ef863f08f2fd7cf7aae7bf340d9
varun2425 <varunmahi030@gmail.com>
2025-11-10 20:40:21 +0530

    js

---
+++ Array-method-18/isarray.js
@@ -0,0 +1,11 @@
+//How to verify given value is array or not?
+let product={}
+let users=[]
+
+console.log(Array.isArray(product))
+
+if (Array.isArray(users)) {
+    console.log("Array")
+} else {
+        console.log("Not Array")
+}
