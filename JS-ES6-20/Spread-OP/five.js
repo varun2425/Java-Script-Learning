419f5cd3b6c4ed4af9f70408d69042cb20b1a2ad
varun2425 <varunmahi030@gmail.com>
2025-11-10 21:17:05 +0530

    js

---
+++ JS-ES6-20/Spread-OP/five.js
@@ -0,0 +1,8 @@
+/*
+    What is spread operator?
+    extract and copy
+    Spread(...) appliable for array and object.
+    1.create new array/object
+    2.merge arrays/objects
+    3.extending array elements/object properties.
+*/
