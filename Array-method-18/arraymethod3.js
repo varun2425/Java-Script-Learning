39b3a727e9da5e257c67d314f7136a23ed939e4d
varun2425 <varunmahi030@gmail.com>
2025-11-10 20:44:49 +0530

    js

---
+++ Array-method-18/arraymethod3.js
@@ -0,0 +1,6 @@
+let enames=["Rahul","Sonia","Priyanka","Modi"]
+//index      0         1       2        3
+console.log(enames.indexOf("Sonia"))    //1
+//return index value of specified element
+
+console.log(enames.indexOf("Rajni"))    //-1
