71e6a3904342f40b1ee438c72ca9ef355517bf84
varun2425 <varunmahi030@gmail.com>
2025-11-10 21:19:46 +0530

    js

---
+++ JS-ES6-20/Spread-OP/seven.js
@@ -0,0 +1,12 @@
+//create new array based on existing array
+let numbers=[11,7,8,8,11]
+let new_numbers=[]
+
+let index=0;
+while(index<=numbers.length-1){
+    new_numbers.push(numbers[index])
+    //new_numbers.push("GM")
+    index++;
+}
+console.log(numbers)
+console.log(new_numbers)
