9f80495f17c441346e54051e5bdc437b3cd7403b
varun2425 <varunmahi030@gmail.com>
2025-11-10 21:22:05 +0530

    js

---
+++ JS-ES6-20/Spread-OP/nine.js
@@ -0,0 +1,10 @@
+/*
+Create new Array's / create new Object using spread op
+*/
+let numbers=[11,7,8,232,8]
+let new_numbers=[...numbers]
+console.log(new_numbers)
+
+let emp={eid:101,ename:"Rahul",esal:45000}
+let user={...emp}
+console.log(user)
