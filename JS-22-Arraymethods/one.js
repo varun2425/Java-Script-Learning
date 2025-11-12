38ac2c0523e2afb3e4def6e01cdf8782eabe0372
varun2425 <varunmahi030@gmail.com>
2025-11-12 10:14:26 +0530

    js

---
+++ JS-22-Arraymethods/one.js
@@ -0,0 +1,9 @@
+let employees =[
+    {eid:101,ename:"Varun",gender:"Male"},
+    {eid:102,ename:"Karthik",gender:"Male"},
+    {eid:103,ename:"Renuka",gender:"Female"},
+    {eid:104,ename:"Annu",gender:"Female"},
+]
+
+let male_Employees=employees.filter(e=>e.gender=="Male")
+console.log(male_Employees)
