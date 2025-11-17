8fb8d0cfae1524539b376c91e392ad6e090c26a8
varun2425 <varunmahi030@gmail.com>
2025-11-17 15:44:24 +0530

    js

--- JS-31-asyncamwait/data.js
+++ JS-31-asyncamwait/data.js
@@ -1,5 +1,36 @@
-let employee=[
+let employees=[
+    {eid:101,ename:"Rahul",esal:45000},
+    {eid:102,ename:"Sonia",esal:55000},
 ]
 let createEmployee=(emp)=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            let flag=true;
+            if(flag){
+                resolve("Data Inserted")
+                employees.push(emp)
+            }else{
+                reject("Failed")
+            }
+        },4000)
+    });
 }
-let 
+let getEmployees=()=>{
+    setTimeout(()=>{
+        let rows="";
+        for(let emp of employees){
+            rows=rows+`<tr>
+                        <td>${emp.eid}</td>
+                        <td>${emp.ename}</td>
+                        <td>${emp.esal}</td>
+                       </tr>
+                       `
+        }
+        document.getElementById('tb_data').innerHTML=rows
+    },1000)
+}
+let exec=async()=>{
+    await createEmployee({eid:103,ename:"Priya",esal:65000})
+    getEmployees()
+}
+exec()
